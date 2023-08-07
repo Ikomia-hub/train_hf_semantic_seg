a3e24287cd7989ab4c43713cbe1c6e5568b869b4
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 10:17:28 +0200

    Fix test file

--- train_hf_semantic_seg_test.py
+++ train_hf_semantic_seg_test.py
@@ -8,9 +8,9 @@ def test(t, data_dict):
     logger.info("===== Test::train Hugging Face semantic segmentation =====")
     input_dataset = t.get_input(0)
     params = t.get_parameters()
-    params["epochs"] = 2
-    params["batch_size"] = 1
-    params["test_percentage"] = 0.5
+    params["epochs"] = "2"
+    params["batch_size"] = "1"
+    params["test_percentage"] = "0.5"
     t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["semantic_segmentation"]["dataset_coco"])
     yield run_for_test(t)
